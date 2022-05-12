71da1271cce7db9ff84f0901fb885c017582aef7
Patrick Ostie <patrick.ostie@stroeer.de>
2022-05-12 14:42:09 +0200

    refactor: put default description into locals

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
+locals {
+  default_event_rule_description = "Invokes ${var.function.name} ${each.key}"
+}
+
 resource "aws_cloudwatch_event_rule" "schedule" {
   for_each = var.schedules
   name                = "${var.function.name}-${each.key}-schedule"
-  description         = each.value.description != null ? "${each.value.description}" : "Invokes ${var.function.name} ${each.key}"
+  description         = each.value.description != null ? each.value.description : local.default_event_rule_description
   schedule_expression = each.value.schedule
 }
 
