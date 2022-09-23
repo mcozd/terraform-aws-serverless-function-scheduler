307fa272a89d618a1db2ebfd13162d7228adcde2
Patrick Ostie <patrick.ostie@stroeer.de>
2022-09-23 12:21:59 +0200

    feat: allow to set inputs on scheduled functions for every defined schedule

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 resource "aws_cloudwatch_event_rule" "schedule" {
-  for_each = var.schedules
+  for_each            = var.schedules
   name                = "${var.function.function_name}-${each.key}-schedule"
   description         = each.value.description != null ? each.value.description : "Invokes ${var.function.function_name} ${each.key}"
   schedule_expression = each.value.schedule
 }
 
 resource "aws_cloudwatch_event_target" "schedule" {
-  for_each = var.schedules
+  for_each  = var.schedules
   rule      = aws_cloudwatch_event_rule.schedule[each.key].name
   target_id = "lambda"
   arn       = var.function.arn
+  input     = each.value.input != null ? each.value.input : null
 }
 
 resource "aws_lambda_permission" "schedule" {
-  for_each = var.schedules
+  for_each      = var.schedules
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
   function_name = var.function.function_name
--- variables.tf
+++ variables.tf
@@ -1,12 +1,13 @@
 variable "function" {
-  type = any
+  type        = any
   description = "The lambda function that should be called based on the provided schedule."
 }
 
 variable "schedules" {
   type = map(object({
-    schedule = string
+    schedule    = string
     description = optional(string)
+    input       = optional(string)
   }))
-  description = "Schedules when your function should be invoked. Uses the schedule expression syntax of cloudwatch event rules(cron or rate)."
+  description = "Schedules when your function should be invoked. Uses the schedule expression syntax of cloudwatch event rules(cron or rate). You can optionally pass input to the invoked function. This input must be valid JSON."
 }
