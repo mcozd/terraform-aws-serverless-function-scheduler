3c3921586d552d93a65248742a84b70c791ede0f
Patrick Ostie <patrick.ostie@stroeer.de>
2023-03-05 00:44:23 +0100

    refactor: add defaults for optional args in new syntax

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "function" {
   type        = any
   description = "The lambda function that should be called based on the provided schedule."
-  nullable = false
+  nullable    = false
 }
 
 variable "schedules" {
   type = map(object({
     schedule    = string
-    description = optional(string)
-    input       = optional(string)
+    description = optional(string, null)
+    input       = optional(string, null)
   }))
   description = "Schedules when your function should be invoked. Uses the schedule expression syntax of cloudwatch event rules(cron or rate). You can optionally pass input to the invoked function. This input must be valid JSON."
-  nullable = false
+  nullable    = false
 }
